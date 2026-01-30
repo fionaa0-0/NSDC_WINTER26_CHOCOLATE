f5df2f18915e436884261fb41efe4a6ed21f3692
fionarhine <wangsawidjajafiona@gmail.com>
2026-01-30 15:42:40 -0800

    comments + save clean data

--- 🍫_main_file.py
+++ 🍫_main_file.py
@@ -4,8 +4,15 @@
 
 import pandas as pd
 
+#read in data + get its info
 data = pd.read_csv("flavors_of_cacao.csv")
 data.head()
 data.info()
 
+#drop redundant columns
 data = data.drop(columns=["REF", "Review\nDate", "Bean\nType"])
+data.head()
+
+#save the cleaned data to csv file
+data.to_csv("cleaned_dataset_flavors_of_cacao.csv", index=False)
+
