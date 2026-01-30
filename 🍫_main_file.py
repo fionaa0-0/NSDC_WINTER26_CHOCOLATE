52e8d7394d4b41e25dfa8c570d6fe01ab0f6be8b
fionarhine <wangsawidjajafiona@gmail.com>
2026-01-30 15:54:50 -0800

    reading in 2nd dataset (choco sales)

--- 🍫_main_file.py
+++ 🍫_main_file.py
@@ -13,6 +13,18 @@ data.info()
 data = data.drop(columns=["REF", "Review\nDate", "Bean\nType"])
 data.head()
 
+whitespace_only = (data["Broad Bean\nOrigin"].astype(str).str.strip() == "").sum()
+whitespace_only
+
+#since there are only 73 out of our thousand obs that are NA, (less than 5% of our data), we can go ahead and just delete those rows 
+#now we want to delete the rows 
+data = data[data["Broad Bean\nOrigin"].astype(str).str.strip() != ""]
+data.info()
+
 #save the cleaned data to csv file
 data.to_csv("cleaned_dataset_flavors_of_cacao.csv", index=False)
 
+#now we want to read in the 2nd dataset!
+data2 = pd.read_csv("Chocolate Sales.csv")
+data.head()
+data.info()
