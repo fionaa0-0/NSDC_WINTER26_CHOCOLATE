2f7913f29737152bf7c55becbb6252bb04a266d1
fionarhine <wangsawidjajafiona@gmail.com>
2026-01-30 15:39:24 -0800

    reading in dataset
    read in data + drop REF and review date columns

--- 🍫_main_file.py
+++ 🍫_main_file.py
@@ -1,3 +1,11 @@
 #HI GUYS lets check if this works (each person write their name below as a comment, commit, then push to main!)
 #fiona
 #kavya
+
+import pandas as pd
+
+data = pd.read_csv("flavors_of_cacao.csv")
+data.head()
+data.info()
+
+data = data.drop(columns=["REF", "Review\nDate", "Bean\nType"])
